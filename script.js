6e84be7be8e9b8f3984a02e002b75f25807815fb
Tognetti <2503.gui@gmail.com>
2022-08-23 16:30:36 -0300

    Finalização do JS, mudança de alguns IDs para o execComand funcionar corretamente. Funcionando semerros.

--- script.js
+++ script.js
@@ -1,41 +1,88 @@
-let opcaobotao = document.querySelectorAll('.opcaobotao');
-let opcaobotaoavancado = document.querySelectorAll('.opcaobotaoavancado');
-let nomedafonte = document.getElementById('nomedafonte');
-let tamanhodafonte = document.getElementById('tamanhodafonte');
-let areadotexto = document.getElementById('areadotexto');
-let crialink = document.getElementById('crialink')
-let opcaobotaoalinhamento = document.querySelectorAll('.opcaobotaoalinhamento');
-let opcaobotaorecuo = document.querySelectorAll('.opcaobotaorecuo');
-let formatacaobloco = document.querySelectorAll('.formatacaobloco');
-let opcaobotaotamanho = document.querySelectorAll('.opcaobotaotamanho');
+let botao = document.querySelectorAll(".botao")
+let botaoavancado = document.querySelectorAll(".avancado");
+let nomefonte = document.getElementById("fontName");
+let tamanhofonte = document.getElementById("fontSize");
+let texto = document.getElementById("areadotexto");
+let botaolink = document.getElementById("createLink")
+let botaoalinhamento = document.querySelectorAll(".alinhamento");
+let botaorecuo = document.querySelectorAll(".recuo");
+let botaoformatacao = document.querySelectorAll(".formatacao");
+let botaotamanho = document.querySelectorAll(".tamanho");
+let listafontes = ["Arial", "Verdana", "Times New Roman", "Garamond", "Georgia", "Courier New", "Cursive",];
 
-let listadefontes = ["Arial", "Verdana", "Times New Roman", "Garamond", "Georgia", "Courier New", "Curisve"];
 
 const inicializa = () => {
-  marcatexto(opcaobotaoalinhamento, true);
-  marcatexto(opcaobotaorecuo, true);
-  marcatexto(formatacaobloco, false);
-  marcatexto(opcaobotaotamanho, true);
-};
+  ligabotao(botaoalinhamento, true);
+  ligabotao(botaorecuo, true);
+  ligabotao(botaoformatacao, false);
+  ligabotao(botaotamanho, true);
 
-const marcatexto = (nomedaclasse, precisaremover) =>{
-  nomedaclasse.forEach((button) => {
-    button.addEventListener("click", () =>{
-      if (precisaremover) {
-        let jaligado = false;
-
-        if (button.classList.contains("ligado")) {
-            jaligado = true;
-        }
-        removemarcatexto(nomedaclasse);
-        if (!jaligado) {
-          button.classList.add("ligado");
-        }
-      }
-      else {
-        button.classList.toggle("ligado");
-      }
-    });
+  listafontes.map((value) => {
+  let opcao = document.createElement("option");
+  opcao.value = value;
+  opcao.innerHTML = value;
+  nomefonte.appendChild(opcao);
   });
 
-}
+  for (let i = 1; i <= 7; i++) {
+    let opcao = document.createElement("option");
+    opcao.value = i;
+    opcao.innerHTML = i;
+    tamanhofonte.appendChild(opcao);
+  }
+  tamanhofonte.value = 3;
+};
+
+const mudatexto = (command, defaultUi, value) => {
+  document.execCommand(command, defaultUi, value);
+};
+
+botao.forEach((button) => {
+  button.addEventListener("click", () => {
+    mudatexto(button.id, false, null);
+  });
+});
+
+botaoavancado.forEach((button) => {
+  button.addEventListener("change",()=>{
+    mudatexto(button.id, false, button.value);
+  });
+});
+
+botaolink.addEventListener("click", ()=>{
+  let linkusuario = prompt("Digite a URL");
+  if (/http/i.test(linkusuario)) {
+    mudatexto(botaolink.id,false,linkusuario);
+  }
+  else {
+    linkusuario = "http://" + linkusuario;
+    mudatexto(botaolink.id,false,linkusuario);
+  }
+});
+
+const ligabotao =  (nomeclasse, precisadesligar) => {
+  nomeclasse.forEach((button) => {
+        button.addEventListener("click", () =>{
+            if (precisadesligar) {
+                  let estaligado = false;
+                    if (button.classList.contains("ligado")) {
+                        estaligado = true;
+                      }
+                desligabotao(nomeclasse);
+                if (!estaligado) {
+                      button.classList.add("ligado");
+                }
+            }
+            else {
+                button.classList.toggle("ligado");
+            }
+        });
+  });
+};
+
+const desligabotao = (nomeclasse) =>{
+  nomeclasse.forEach((button) => {
+    button.classList.remove("ligado")
+  });
+};
+window.onload = inicializa();
